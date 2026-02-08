d9fb84f527b282bdde848bc30047bf784cc3670c
Ngoc-Nhan <nhanvtn7501@ut.edu.vn>
2026-02-08 11:44:07 +0700

    chinh

--- lib/button_value.dart
+++ lib/button_value.dart
@@ -1,32 +1,43 @@
 class Btn {
+  static const String del = "D";
   static const String ac = "AC";
-  static const String del = "DEL";
   static const String percent = "%";
-  static const String divide = "÷";
-  static const String multiply = "×";
+  static const String divide = "/";
+  static const String seven = "7";
+  static const String eight = "8";
+  static const String nine = "9";
+  static const String multiply = "*";
+  static const String four = "4";
+  static const String five = "5";
+  static const String six = "6";
   static const String minus = "-";
+  static const String one = "1";
+  static const String two = "2";
+  static const String three = "3";
   static const String plus = "+";
-  static const String equal = "=";
+  static const String convert = "⇆";
+  static const String zero = "0";
   static const String dot = ".";
-
+  static const String equal = "=";
   static const List<String> buttonValues = [
     ac,
     del,
     percent,
     divide,
-    "7",
-    "8",
-    "9",
+    seven,
+    eight,
+    nine,
     multiply,
-    "4",
-    "5",
-    "6",
+    four,
+    five,
+    six,
     minus,
-    "1",
-    "2",
-    "3",
+    one,
+    two,
+    three,
     plus,
-    "0",
+    convert,
+    zero,
     dot,
     equal,
   ];
